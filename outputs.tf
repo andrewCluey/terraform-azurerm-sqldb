c3981a02dd4726550e465a5e3540b3ee1ce09300
ASC IT Solutions <andrew@asc-solutions.co.uk>
2020-09-23 13:48:50 +0100

    added new output attributes for SQL name and ID

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "sql_pw" {
     value = random_string.password.result
 }
+
+output "sql_server_name" {
+  value = azurerm_sql_server.sql_svr.name
+}
+
+output "sql_server_id" {
+  value = azurerm_sql_server.sql_svr.id
+}
+
+output "sqdb_name" {
+  value = azurerm_sql_database.default_db.name
+}
+
+output "sqldb_id" {
+  value = azurerm_sql_database.default_db.id
+}
